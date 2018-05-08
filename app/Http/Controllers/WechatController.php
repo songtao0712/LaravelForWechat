e700d07bbb684113541687486cb6b4fb6727854c
songtao <478938840@qq.com>
2018-05-08 22:16:28 +0800

    no message

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -5,15 +5,15 @@
  * Date: 2018/5/8
  * Time: 20:36
  */
-namespace App\Http\Controllers\Wechat;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
 class WechatController extends Controller
 {
 
-    public function serve()
+    public function test()
     {
-        echo $_GET['echostr'];
+        echo 123;
     }
 }
